2a7c5eef2ba1cf5dae2b5b31d846b1c9d28efabe
Paul Bonser <pib@paulbonser.com>
2011-02-02 10:26:37 -0600

    insert, search working for single-node trees

--- slobtree.py
+++ slobtree.py
@@ -5,24 +5,26 @@ import json
 
 class Index(object):
     def __init__(self, filename):
-        f = self.f = open(filename, 'ab+')
+        self.f = open(filename, 'ab+')
+        self.root = None
+        self._read_root()
 
+    def _read_root(self):
+        self.f.seek(0, os.SEEK_END)
         offset = 0
         last_nl = -1
-        while last_nl < 0 and f.tell() > 0:
+        while last_nl < 0 and self.f.tell() > 0:
             offset += 1024
             try:
-                f.seek(-offset, os.SEEK_END)
+                self.f.seek(-offset, os.SEEK_END)
             except IOError, e:
                 if e.errno != errno.EINVAL:
                     raise
-                f.seek(0)
-            content = f.read(offset)
+                self.f.seek(0)
+            content = self.f.read(offset)
             last_nl = content.rfind('\n')
-        if last_nl > 0:
+        if last_nl > -1:
             self.root = json.loads(content[last_nl:])
-        else:
-            self.root = None
 
     def insert(self, key, val):
         self.f.write('\n')
@@ -30,4 +32,4 @@ class Index(object):
         self.f.flush()
 
     def search(self, key):
-        return 'bar'
+        return self.root.get('data', {}).get(key)
